68ebdcd4222133f7111af043c437beb1ecb60970
thebrandonhoward <thebrandonhoward@outlook.com>
2022-10-13 07:07:12 -0500

    Adds security table repositories

---
+++ src/main/java/com/example/jwtdemo/repo/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.jwtdemo.repo;
+
+import com.example.jwtdemo.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Role findRoleByName(String name);
+}
