c2ced6f02f00888441525a7bba683a9ea48ecb81
thebrandonhoward <thebrandonhoward@outlook.com>
2022-10-13 08:58:32 -0500

    Adds api implementations

--- src/main/java/com/example/jwtdemo/api/UserResource.java
+++ src/main/java/com/example/jwtdemo/api/UserResource.java
@@ -1,13 +1,14 @@
 package com.example.jwtdemo.api;
 
+import com.example.jwtdemo.domain.Role;
 import com.example.jwtdemo.domain.User;
 import com.example.jwtdemo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequestMapping("/api")
@@ -19,5 +20,23 @@ public class UserResource {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        return ResponseEntity.created(URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString()))
+                .body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        return ResponseEntity.created(URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString()))
+                .body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser/{username}/{rolename}")
+    public ResponseEntity<?> saveRole(@PathVariable String username, @PathVariable String rolename){
+        userService.addRoleToUser(username, rolename);
+        return ResponseEntity.ok().build();
     }
 }
