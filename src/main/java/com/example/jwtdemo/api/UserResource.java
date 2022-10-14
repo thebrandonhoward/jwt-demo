86133d163cd3df32c917abb282aa72d23ae504bd
thebrandonhoward <thebrandonhoward@outlook.com>
2022-10-13 23:23:31 -0500

    Adds refresh token handling

--- src/main/java/com/example/jwtdemo/api/UserResource.java
+++ src/main/java/com/example/jwtdemo/api/UserResource.java
@@ -1,19 +1,33 @@
 package com.example.jwtdemo.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.jwtdemo.domain.Role;
 import com.example.jwtdemo.domain.User;
 import com.example.jwtdemo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class UserResource {
     private final UserService userService;
 
@@ -38,5 +52,66 @@ public class UserResource {
     public ResponseEntity<?> saveRole(@PathVariable String username, @PathVariable String rolename){
         userService.addRoleToUser(username, rolename);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String SECRET = Base64.getEncoder().encodeToString("jwtdemo".getBytes());
+
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+        if(Objects.nonNull(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String token = authorizationHeader.substring("Bearer ".length());
+                //used to sign refresh and access token
+                Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+                JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+
+                DecodedJWT decodedJWT = jwtVerifier.verify(token);
+
+                String username = decodedJWT.getSubject();
+                User user = userService.getUser(username);
+                //create access token
+                String accessToken
+                        = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRoles().stream().map(role -> role.getName()).toList())
+                        .sign(algorithm);
+
+                //create refresh token
+                String refreshToken
+                        = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .sign(algorithm);
+
+                //to return a body
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(),tokens);
+
+            }
+            catch (Exception e) {
+                log.error(e.getMessage(), e);
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                //response.sendError(FORBIDDEN.value());
+                //to return a body
+                Map<String, String> error = new HashMap<>();
+                error.put("error", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }
+        else {
+            throw new RuntimeException("Refresh token missing.");
+        }
     }
 }
--- src/main/java/com/example/jwtdemo/security/SecurityConfiguration.java
+++ src/main/java/com/example/jwtdemo/security/SecurityConfiguration.java
@@ -49,6 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/api/login/**","/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/save/**").hasAnyAuthority("ROLE_SUPER_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
